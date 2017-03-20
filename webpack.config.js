572a1068ab8d46240d31c80e542baaa56012407c
Daniel Böök <Daniel.book.93@gmail.com>
2017-03-20 07:40:38 +0100

    Renamed files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
 
-    entry: './dev/js/entry.js',
+    entry: './dev/js/index.js',
     devServer: {
         inline: true,
         contentBase: './src',
