6d4c896d91f92f3aec2242a830991924c43a5eef
Daniel Böök <Danbo324@student.liu.se>
2017-09-20 15:34:29 +0200

    Migrated to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,44 @@
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-
-    entry: './dev/js/index.js',
-    devServer: {
-        inline: true,
-        contentBase: './src',
-        port: 3000,
-        historyApiFallback: true
-    },
-    module: {
-        loaders : [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test: /\.scss/,
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    output: {
-        path: 'src',
-        filename: 'js/bundle.min.js'
+  entry: './dev/js/index.js',
+  devServer: {
+    inline: true,
+    contentBase: './src',
+    port: 3000,
+    historyApiFallback: true
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+          options: {
+            includePaths: ['src/img/slides']
+          }
+        }]
+      }
+    ]
+  },
+  output: {
+    filename: 'js/bundle.min.js',
+    path: path.resolve(__dirname, 'src')
+  },
+  resolve: {
+    alias: {
+      'assets': path.resolve('src/')
     }
+  }
 };
